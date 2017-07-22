2fb7d50d130a4222ed605ab447efbfda008b8a6a
Jamie Terry <jnterry@ntlworld.com>
2017-07-22 16:25:03 +0100

    Setup all-the-icons for use by neotree

--- config/packages/neotree.el
+++ config/packages/neotree.el
@@ -1,10 +1,11 @@
+(add-to-list 'load-path "~/.emacs.d/config/ext/all-the-icons/")
+(require 'all-the-icons)
+
 (load "ext/neotree")
 
 (global-set-key [f8] 'neotree-toogle)
 
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
 ;; Start with neotree open by default
 (neotree)
-
-(setq neo-theme 'arrow)
-
-;; (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
--- init.el
+++ init.el
@@ -1,4 +1,12 @@
-(add-to-list 'load-path "~/.emacs.d/config")
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/config/")
+(add-to-list 'load-path "~/.emacs.d/config/ext/")
 
 (load "theme")
 
