6e4df66b5a47801a68e47622b87584528f379ba9
Jamie Terry <jnterry@ntlworld.com>
2017-07-22 20:28:49 +0100

    Add smarter move to begining of line tweak

---
+++ config/tweaks/smarter-move-beginning-of-line.el
@@ -0,0 +1,33 @@
+;; By default:
+;;  - C-a goes right to start of line
+;;  - M-m goes to first non-whitespace char of line
+;; This remaps C-a to first go to first non-whitespace char, then to start of line
+;;
+;; Taken from: http://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginning-of-a-line/
+
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line]
+                'smarter-move-beginning-of-line)
