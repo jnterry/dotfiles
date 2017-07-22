2cc8f74c568a968a9d1c8997c00039edc0fc6625
Jamie Terry <jnterry@ntlworld.com>
2017-07-22 19:25:45 +0100

    Enable line numbers

--- config/misc-tweaks.el
+++ config/misc-tweaks.el
@@ -1,8 +1,11 @@
 (setq user-full-name    "Jamie Terry"
       user-mail-address "jnterry@ntlworld.com")
 
-;; change all prompts to y or n
+;; Change all prompts to y or n
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;;have buffers reload when files are changed on disk
+;; Have buffers reload when files are changed on disk
 (global-auto-revert-mode t)
+
+;; Enable line numbers
+(global-linum-mode t)
