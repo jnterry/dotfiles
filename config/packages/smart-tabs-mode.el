d1edc794dc2802cd4b5d4e1026e399a8cf4d35b3
Jamie Terry <jnterry@ntlworld.com>
2017-07-22 19:22:10 +0100

    Add smart-tabs-mode package

---
+++ config/packages/smart-tabs-mode.el
@@ -0,0 +1,11 @@
+;; Use tabs for indentation, and spaces for alignment
+(use-package smart-tabs-mode)
+
+(setq-default tab-width 4)
+(setq         tab-width 4)
+
+(setq-default indent-tabs-mode 1)
+(setq         indent-tabs-mode 1)
+
+;; Set languages to use smart tabs mode
+(smart-tabs-insinuate 'c 'c++ 'java)
