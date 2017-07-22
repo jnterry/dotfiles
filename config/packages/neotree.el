9f1e5ff7ce4ec0b29856d0e5cbbea162b24aa3eb
Jamie Terry <jnterry@ntlworld.com>
2017-07-22 16:42:57 +0100

    Configure neotree to show hidden files

--- config/packages/neotree.el
+++ config/packages/neotree.el
@@ -5,7 +5,19 @@
 
 (global-set-key [f8] 'neotree-toogle)
 
+;; Use graphical icons for file types/directories in graphics mode,
+;; else us standard collapseable arrows
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
+;; Overwrite default hidden regex to show most hidden files, but not .git
+(setq neo-hidden-regexp-list
+      '(
+	"^\\.git$" ;; Git dir
+	"~$"      ;; emacs backup files
+	"^#.*#$"  ;; emacs backup files
+	"\\.elc$" ;; compiled el files
+	)
+      )
 
 ;; Start with neotree open by default
 (neotree)
