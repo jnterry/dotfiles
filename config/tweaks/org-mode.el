12764aa78428c0dab539363c5eef43c38b075f5e
Jamie Terry <jnterry@ntlworld.com>
2017-07-23 13:07:03 +0100

    More org tweaks

--- config/tweaks/org-mode.el
+++ config/tweaks/org-mode.el
@@ -1,7 +1,20 @@
 ;; Tweaks to ORG mode
 
+;; Drag and drop images into org-mode
+(use-package org-download
+  :ensure t
+  :defer 2)
+
 (setq org-latex-create-formula-image-program 'dvipng)
 
+;; Only display 300px of wide images
+(setq org-image-actual-width '(300))
+
+;; Display code blocks with usual syntax highlighting
+(setq org-src-fontify-natively t)
+
+;; Lets tab work as normal in code blocks
+(setq org-src-tab-acts-natively t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; org mode script blocks
