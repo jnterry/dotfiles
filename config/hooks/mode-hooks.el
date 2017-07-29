7d5d83fbc6512362088e61510a40e4f5c96ca02c
Jamie Terry <jnterry@ntlworld.com>
2017-07-29 10:29:18 +0100

    Enable flyspell in comments of programming languages and everywhere in text languages

--- config/hooks/mode-hooks.el
+++ config/hooks/mode-hooks.el
@@ -1,4 +1,34 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Spelling Checks                                                        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Enable spell check of comments in following language modes
+(dolist (hook '(lisp-mode-hook
+                emacs-lisp-mode-hook
+                yaml-mode
+                shell-mode-hook
+                php-mode-hook
+                html-mode-hook
+                haskel-mode-hook
+                js-mode-hook
+				js2-mode-hook
+				c-mode-hook
+				c++-mode-hook
+			   )
+		 )
+  (add-hook hook 'flyspell-prog-mode)
+)
+
+;; Enable spell check of everything in following language modes
+(dolist (hook '(org-mode-hook
+				markdown-mode-hook
+			   )
+		 )
+  (add-hook hook 'flyspell-mode)
+)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; fix indentation of enum class in c++                                   ;;
 ;; taken from: http://stackoverflow.com/questions/6497374/                ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
