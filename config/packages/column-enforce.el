de3896063e3bf786747d86a32dc9fe3451d4b1a9
Jamie Terry <jnterry@ntlworld.com>
2017-09-15 12:45:13 +0100

    Fix warnings with column enforce mode by using packaged version

--- config/packages/column-enforce.el
+++ config/packages/column-enforce.el
@@ -1,5 +1,10 @@
 (load "ext/column-enforce-mode")
 
-(setq column-enforce-column 80)
+(use-package `
 
-(global-column-enforce-mode t)
+(use-package column-enforce
+  :config
+	(setq column-enforce-column 80)
+	:init
+	(global-column-enforce-mode t)
+)
