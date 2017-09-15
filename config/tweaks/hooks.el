cabeb3eb14e661a3cbea25fdd3fbc7ea71b0953d
Jamie Terry <jnterry@ntlworld.com>
2017-09-15 12:14:02 +0100

    Remove broken startup hook

--- config/tweaks/hooks.el
+++ config/tweaks/hooks.el
@@ -1,10 +1,2 @@
-(add-hook 'emacs-startup-hook
-		  (lambda () (
-					  (global-visual-line-mode) ;; Always use visual-line-mode
-					  )
-			)
-		  )
-
-
 ;; Delete whitespace at end of lines
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
