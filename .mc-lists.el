05d794b6582380b9545981204052eaa8f48a30df
Jamie Terry <jnterry@ntlworld.com>
2017-09-22 17:00:58 +0100

    Update multi-cursor command list

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,19 +3,22 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+				beginning-of-visual-line
 				electric-newline-and-maybe-indent
 				end-of-visual-line
 				indent-for-tab-command
 				keyboard-escape-quit
 				org-end-of-line
+				org-self-insert-command
 				overwrite-mode
 				sh-assignment
+				smarter-move-beginning-of-line
 				yaml-electric-backspace
 				yaml-electric-dash-and-dot
-				beginning-of-visual-line
 				))
 
 (setq mc/cmds-to-run-once
       '(
 				mouse-set-region
+				org-insert-heading-respect-content
 				))
