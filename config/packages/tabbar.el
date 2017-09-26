14d9d4d59059ecf353ed6562f37c9e06d82b4d14
Jamie Terry <jnterry@ntlworld.com>
2017-09-26 22:40:11 +0100

    Change keybindings for tabbar to avoid conflict with org mode changing list/heading indent level

--- config/packages/tabbar.el
+++ config/packages/tabbar.el
@@ -10,20 +10,21 @@
 (tabbar-mode)
 
 ;; Enable hotkeys for switching between tabs
-(global-set-key [M-left] 'tabbar-backward-tab)
-(global-set-key [M-right] 'tabbar-forward-tab)
+(global-set-key (kbd "M-[") 'tabbar-backward-tab)
+(global-set-key (kbd "M-]") 'tabbar-forward-tab )
 
 ;; Make all tabs be displayed in single group, hence can see all open buffers at once
 ;; Groups:
 ;;  - user  - all user created buffers
 ;;  - emacs - emacs created buffers (scratch, messages)
 (defun my-tabbar-buffer-groups ()
-  (list (cond ((string=      "*shell*"    (buffer-name)       ) "user" ) ;; put emacs shells in user group
-			  ((string=      "*terminal*" (buffer-name)       ) "user" ) ;;  "      "      "      "      "
-			  ((string-equal "*" (substring (buffer-name) 0 1)) "emacs") ;; other emacs buffers in emacs group
-			  ((eq major-mode 'dired-mode                     ) "emacs") ;;  "      "      "      "      "
-			  (t                                                "user" ) ;; everything else in user group
-			  )
+  (list (cond
+				 ((string=      "*shell*"    (buffer-name)       ) "user" ) ;; put emacs shells in user group
+				 ((string=      "*terminal*" (buffer-name)       ) "user" ) ;;  "      "      "      "      "
+				 ((string-equal "*" (substring (buffer-name) 0 1)) "emacs") ;; other emacs buffers in emacs group
+				 ((eq major-mode 'dired-mode                     ) "emacs") ;;  "      "      "      "      "
+				 (t                                                "user" ) ;; everything else in user group
+				)
 		)
   )
 
