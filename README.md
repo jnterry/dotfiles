b80a93480f0e7c69fd4134395bab5c9898681ece
Jamie Terry <jnterry@ntlworld.com>
2017-12-19 10:50:00 +0000

    Move files common to all hosts into a 'common' directory

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # What
 Contains the .dotfiles that configure various applications.
 
+These files are managed by GNU stow - the creates system links in the correct location that point to the files in this repository.
+
+```./common/``` contains config files that should be installed on all hosts, the other directories in this folder should only be installed on devices with particular hostnames
+
 # To Install
 
 1. Clone the repository
--- install.sh
+++ install.sh
@@ -2,9 +2,29 @@
 #
 # Installs all dotfiles to the appropriate locations
 
+pushd $(dirname "${0}") > /dev/null
+SCRIPTDIR=$(pwd -L)
+popd > /dev/null
+
+#######################################
+# Install common files
+cd ${SCRIPTDIR}/common
 for d in `ls -d */`;
 do
-		printf "Installing '$d'... "
+		printf "Installing %32s : " $d
     ( stow $d --target $HOME )
 		printf "Done\n"
 done
+
+#######################################
+# Install host specific files
+HOSTDIR=${SCRIPTDIR}/$(hostname)
+if [[ -d ${HOSTDIR} ]] ; then
+		cd ${HOSTDIR}
+		for d in `ls -d */`;
+		do
+				printf "Installing %32s : " $d
+				( stow $d --target $HOME )
+				printf "Done\n"
+		done
+fi
