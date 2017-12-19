c6e7ee19cde2198a796f148885049a2579d89741
Jamie Terry <jnterry@ntlworld.com>
2017-12-19 10:28:31 +0000

    Add i3 and polybar config

---
+++ i3/.config/polybar/launch_polybar.sh
@@ -0,0 +1,12 @@
+killall -q polybar
+killall -q comptron
+
+compton &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
