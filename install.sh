41a9db0e49d9bd65193b27f8135727896ca07dcb
Jamie Terry <jnterry@ntlworld.com>
2017-12-23 16:16:39 +0000

    Fix indentation, use stowforce rather than stow

--- install.sh
+++ install.sh
@@ -10,21 +10,21 @@ popd > /dev/null
 # Install common files
 cd ${SCRIPTDIR}/common
 for d in `ls -d */`;
-do
-		printf "Installing %32s : " $d
-    ( stow $d --target $HOME )
-		printf "Done\n"
+do	
+	printf "Installing %32s : " $d
+	( /usr/bin/stowforce $d --target $HOME )
+	printf "Done\n"
 done
 
 #######################################
 # Install host specific files
 HOSTDIR=${SCRIPTDIR}/$(hostname)
 if [[ -d ${HOSTDIR} ]] ; then
-		cd ${HOSTDIR}
-		for d in `ls -d */`;
-		do
-				printf "Installing %32s : " $d
-				( stow $d --target $HOME )
-				printf "Done\n"
-		done
+	cd ${HOSTDIR}
+	for d in `ls -d */`;
+	do
+		printf "Installing %32s : " $d
+		( /usr/bin/stowforce $d --target $HOME )
+		printf "Done\n"
+	done
 fi
