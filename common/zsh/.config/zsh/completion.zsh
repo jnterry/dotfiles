687eb78b2ee18f3e1204c491b1a8968ca642d571
Jamie Terry <jnterry@ntlworld.com>
2017-12-30 11:00:36 +0000

    Enable case insensitive auto-completion for zsh

--- common/zsh/.config/zsh/completion.zsh
+++ common/zsh/.config/zsh/completion.zsh
@@ -1,4 +1,7 @@
 # Config for the zsh completion
 
+# Ignore case in tab completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
 # Highlight the selected option, and use arrow keys to navigate the menu
 zstyle ':completion:*' menu select
