c881e5c4bc3161fb44eb4f771015aa5aa86e7e5c
Jamie Terry <jnterry@ntlworld.com>
2018-03-26 11:24:42 +0100

    Ensure rebuild actually rebuilds the config rather than just setting up state for emacs to rebuild it next time. This means config can now be checked for compile errors from command line

--- common/emacs/.emacs.d/rebuild.sh
+++ common/emacs/.emacs.d/rebuild.sh
@@ -8,12 +8,15 @@ pushd $(dirname "${0}") > /dev/null
 SCRIPTDIR=$(pwd -L)
 popd > /dev/null
 
+# Delete any existing compiled .elc files
 if [[ `find ${SCRIPTDIR}/config | grep .elc` ]] ; then
 		rm `find ${SCRIPTDIR}/config | grep .elc`
 fi
 
+# Refresh the emacs custom file
 rm    ${SCRIPTDIR}/custom.el
 touch ${SCRIPTDIR}/custom.el
 
-emacs --batch \
-			-l "${SCRIPTDIR}/init.el"
+# Make emacs load the config file - this will return 0 if the config could
+# be loaded, but non 0 if an error occurs
+emacs --batch -l "${SCRIPTDIR}/init.el"
