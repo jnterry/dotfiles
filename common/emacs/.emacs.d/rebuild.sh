f72503c481b978bc10d71fb5848a8b5a8e91a27e
Jamie Terry <jnterry@ntlworld.com>
2018-03-26 11:22:39 +0100

    Ensure rebuild script actually rebuilds the config files, rather than just setting up state so emacs does it on next load

--- common/emacs/.emacs.d/rebuild.sh
+++ common/emacs/.emacs.d/rebuild.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
+#
+# Deletes an existing compiled emacs list (elc files) and then rebuilds them
 
-rm `find ~/.emacs.d | grep .elc`
+set -e
 
-rm    ~/.emacs.d/custom.el
-touch ~/.emacs.d/custom.el
+pushd $(dirname "${0}") > /dev/null
+SCRIPTDIR=$(pwd -L)
+popd > /dev/null
+
+if [[ `find ${SCRIPTDIR}/config | grep .elc` ]] ; then
+		rm `find ${SCRIPTDIR}/config | grep .elc`
+fi
+
+rm    ${SCRIPTDIR}/custom.el
+touch ${SCRIPTDIR}/custom.el
+
+emacs --batch \
+			-l "${SCRIPTDIR}/init.el"
