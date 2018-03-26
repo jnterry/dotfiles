9266c61bbdf4e435e9c5f4362eab6b3a6c8a38e8
Jamie Terry <jnterry@ntlworld.com>
2018-03-26 14:26:38 +0100

    Disable web mode if not using modern emacs version

--- common/emacs/.emacs.d/config/packages/lang-modes.el
+++ common/emacs/.emacs.d/config/packages/lang-modes.el
@@ -51,21 +51,22 @@
   :mode "\\.json\\'"
 )
 
-(use-package web-mode
-  :mode (("\\.html\\.erb\\'" . web-mode)
-         ("\\.mustache\\'"   . web-mode)
-         ("\\.jinja\\'"      . web-mode)
-         ("\\.php\\'"        . web-mode)
-				 ("\\.vue\\'"        . web-mode)
-				 ("\\.html\\'"       . web-mode)
-				 ("\\.dot\\'"        . web-mode)
-				)
-  :config
-  (setq web-mode-enable-css-colorization t)
-	(setq web-mode-markup-indent-offset    2)
-	(setq web-mode-css-indent-offset       2)
-	(setq web-mode-code-indent-offset      2)
-)
+(when using-modern-emacs
+	(use-package web-mode
+		:mode (("\\.html\\.erb\\'" . web-mode)
+					 ("\\.mustache\\'"   . web-mode)
+					 ("\\.jinja\\'"      . web-mode)
+					 ("\\.php\\'"        . web-mode)
+					 ("\\.vue\\'"        . web-mode)
+					 ("\\.html\\'"       . web-mode)
+					 ("\\.dot\\'"        . web-mode)
+					 )
+		:config
+		(setq web-mode-enable-css-colorization t)
+		(setq web-mode-markup-indent-offset    2)
+		(setq web-mode-css-indent-offset       2)
+		(setq web-mode-code-indent-offset      2)
+		))
 
 (when using-modern-emacs
 	(use-package less-css-mode
