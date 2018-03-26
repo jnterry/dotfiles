758d0135b794a14e1e98596ed2303e8075841618
Jamie Terry <jnterry@ntlworld.com>
2018-03-26 13:45:29 +0100

    Ensure that org-mode src block export options are only set if we are using org mode 8.0.0 or newer

--- common/emacs/.emacs.d/config/tweaks/org-mode.el
+++ common/emacs/.emacs.d/config/tweaks/org-mode.el
@@ -1,22 +1,38 @@
 ;; Tweaks to ORG mode
 
-;; Ensure latest version of org is installed, rather than that
-;; bundled with emacs
-;; :TODO: enable? does it work if org already installed?
-;; check with org-version command
-;;(use-package org
-;;	:pin gnu
-;;)
 
+(use-package org
+  ;; Ensure latest version of org is installed, rather than that
+  ;; bundled with emacs
+  ;; :TODO: enable? does it work if org already installed?
+  ;; check with org-version command
+  ;;	:pin gnu
+)
+
+;; Determine the major version of the org package, some of this config relies on
+;; a relatively modern version, so we need to check
+(setq org-major-version (string-to-number (nth 0 (split-string org-version "\\."))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Setup images in org files
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; :TODO: drag and drop images into org mode
 ;; Drag and drop images into org-mode
-(use-package org-download
-  :ensure t
-  :defer 2)
+;;(use-package org-download
+;;  :ensure t
+;;  :defer 2)
 
-(setq org-latex-create-formula-image-program 'dvipng)
+;; :TODO: show preview of latex fragments
+;;(setq org-latex-create-formula-image-program 'dvipng)
+
 
 ;; Only display 300px of wide images
 (setq org-image-actual-width '(300))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Setup SRC block editing
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Display code blocks with usual syntax highlighting
 (setq org-src-fontify-natively t)
@@ -24,32 +40,39 @@
 ;; Lets tab work as normal in code blocks
 (setq org-src-tab-acts-natively t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Ensure source blocks are exported nicely to latex and pdf, this includes:
 ;; - tabs that aren't size 0
 ;; - syntax highlighting
 ;; - outline
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Ensure indentation is preserved in exported src blocks
 (setq org-src-preserve-indentation t)
 
-(setq org-latex-listings t) ;; use listings package in code exports
-(add-to-list 'org-latex-packages-alist '("" "listings")) ;; \usepackage listings
-(add-to-list 'org-latex-packages-alist '("dvipsnames" "xcolor"))    ;; \usepackage color (for syntax highlighting)
-(setq org-latex-listings-options
-	    '(("basicstyle" "\\footnotesize")
-	      ("tabsize" "2")
+;; Note, this relies on requiring ox-latex, this is built into org-mode
+;; versions 8.0.0 and above, hence we check the major version first
+(when (>= org-major-version 8)
+	(require 'ox-latex)
+	(setq org-latex-listings t) ;; use listings package in code exports
+	(add-to-list 'org-latex-packages-alist '("" "listings")) ;; \usepackage listings
+	(add-to-list 'org-latex-packages-alist '("dvipsnames" "xcolor"))    ;; \usepackage color (for syntax highlighting)
+	(setq org-latex-listings-options
+				'(("basicstyle" "\\footnotesize")
+					("tabsize" "2")
 
-	      ("keywordstyle" "\\color{NavyBlue}")
-	      ("commentstyle" "\\color{OliveGreen}")
-	      ("stringstyle"  "\\color{Mahogany}")
+					("keywordstyle" "\\color{NavyBlue}")
+					("commentstyle" "\\color{OliveGreen}")
+					("stringstyle"  "\\color{Mahogany}")
 
-	      ;;("numberstyle" "\\tiny\\color{gray}")
-	      ;;("numbers" "left")
-	      ("numbers" "none")
+					;;("numberstyle" "\\tiny\\color{gray}")
+					;;("numbers" "left")
+					("numbers" "none")
 
-	      ("frame" "single")
-	      )
-	    )
+					("frame" "single")
+					)
+				)
+	)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; org mode script blocks
@@ -67,7 +90,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Add characters to expand into #+BEGIN #+END blocks with <c then tab
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (set-variable 'org-structure-template-alist ()) ;; remove defaults
 
 (add-to-list 'org-structure-template-alist
