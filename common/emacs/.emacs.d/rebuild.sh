3e604fb17a56ff0e53349eb073406b1c44e1886e
Jamie Terry <jnterry@ntlworld.com>
2018-03-26 12:11:23 +0100

    Print emacs version for debugging emacs config build

--- common/emacs/.emacs.d/rebuild.sh
+++ common/emacs/.emacs.d/rebuild.sh
@@ -20,4 +20,5 @@ touch ~/.emacs.d/custom.el
 
 # Make emacs load the config file - this will return 0 if the config could
 # be loaded, but non 0 if an error occurs
+emacs --version # print for debugging purposes
 emacs --batch -l "${SCRIPTDIR}/init.el"
