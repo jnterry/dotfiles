f24cea1d24c85986fc53263dc601f34882d890c8
Jamie Terry <jnterry@ntlworld.com>
2018-03-27 22:32:07 +0100

    Modify magit commit log formatting

--- common/emacs/.emacs.d/config/magit.el
+++ common/emacs/.emacs.d/config/magit.el
@@ -1,3 +1,6 @@
 (use-package magit
-	:bind :bind (("C-x g" . magit-status))
-)
+	:bind (("C-x g" . magit-status))
+	:config
+	(setq magit-log-section-arguments '("--graph" "--color" "--decorate" "++order=topo" "-n256"))
+	(setq magit-log-section-commit-count 30)
+	)
