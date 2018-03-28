cd1c153f5d22a27df21ab40c07a0a19f8668bc05
Jamie Terry <jnterry@ntlworld.com>
2018-03-28 16:42:22 +0100

    Fix highlighting of custom keywords

--- common/emacs/.emacs.d/config/highlight.el
+++ common/emacs/.emacs.d/config/highlight.el
@@ -8,9 +8,9 @@
 (make-face 'font-lock-my-opt-face )
 (make-face 'font-lock-my-comp-face)
 
-(modify-face 'font-lock-my-todo-face "red"    nil false t t nil nil nil)
-(modify-face 'font-lock-my-opt-face  "yellow" nil false t t nil nil nil)
-(modify-face 'font-lock-my-comp-face "cyan"   nil false t t nil nil nil)
+(modify-face 'font-lock-my-todo-face "red"    nil nil t t nil nil nil)
+(modify-face 'font-lock-my-opt-face  "yellow" nil nil t t nil nil nil)
+(modify-face 'font-lock-my-comp-face "cyan"   nil nil t t nil nil nil)
 
 (mapc
  ;; Function which adds the keywords to 'mode;
@@ -20,7 +20,7 @@
 		'((":\\(TODO\\):" 1 'font-lock-my-todo-face t)
 			(":\\(OPT\\):"  1 'font-lock-my-opt-face  t)
 			(":\\(COMP\\):" 1 'font-lock-my-comp-face t)
-			))))
+			)))
 
  ;; List of modes to apply the function to
  '(c++-mode
