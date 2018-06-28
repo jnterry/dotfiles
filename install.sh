a5d2473e719dea8729e706f25a2fbcb31568ea35
Jamie Terry <jnterry@ntlworld.com>
2018-06-28 17:23:09 +0100

    Remove reliance of install script on $HOME env var being set
    This should fix script being ran as part of puppet setup, as puppet does not set enviroment variables

--- install.sh
+++ install.sh
@@ -5,6 +5,13 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTDIR=$(dirname $SCRIPT)
 
+# We don't want to rely on $HOME being set as this may be run as part of
+# automatic system setup when enviroment variables are not present
+#
+# Hence we are going to detect the home directory of the user this script
+# is being ran as, and install to there
+TARGET=$(eval echo "~$(whoami)")
+
 STOW_CMD='/usr/bin/stow'
 if [ -f '/usr/bin/stowforce' ] ; then
 	STOW_CMD='/usr/bin/stowforce'
@@ -14,9 +21,9 @@ fi
 # Install common files
 cd ${SCRIPTDIR}/common
 for d in `ls -d */`;
-do	
+do
 	printf "Installing %32s : " $d
-	$STOW_CMD $d --target $HOME
+	$STOW_CMD $d --target $TARGET
 	printf "Done\n"
 done
 
@@ -28,7 +35,7 @@ if [ -d ${HOSTDIR} ] ; then
 	for d in `ls -d */`;
 	do
 		printf "Installing %32s : " $d
-		$STOW_CMD $d --target $HOME
+		$STOW_CMD $d --target $TARGET
 		printf "Done\n"
 	done
 fi
