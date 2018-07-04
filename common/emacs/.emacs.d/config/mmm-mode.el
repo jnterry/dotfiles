01f303a3744ef07dfa10377ebe6c704a2fe7b496
Jamie Terry <jnterry@ntlworld.com>
2018-07-04 16:56:54 +0100

    Add mmm-mode config enabling editing of org-mode inside code
    Also add orgblk abbreviation to expand to the org-mode block markers.

---
+++ common/emacs/.emacs.d/config/mmm-mode.el
@@ -0,0 +1,31 @@
+;; Set up of mmm-mode (multiple major mode in single buffer)
+
+(use-package mmm-mode
+	;; mmm-mode relies on this non-standard library, load before mmm-mode by putting in init
+	:init (require 'cl)
+	:config
+
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;; org-mode config
+
+	;; Add class for blocks of org-mode within the tags <<@org @>>
+	(mmm-add-classes
+	 '((org-blocks
+			:submode org-mode
+			:front "<<@org"
+			:back  "@>>"
+			)))
+
+	;; Enable org-blocks in the following major modes
+	(add-to-list 'mmm-mode-ext-classes-alist '(puppet-mode     nil org-blocks))
+	;;(add-to-list 'mmm-mode-ext-classes-alist '(c++-mode        nil org-blocks))
+
+	(add-hook 'c-mode-common-hook (lambda()()))
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	;; Always enable mmm-minor-mode if we are in a buffer which might be able to use it
+	(setq mmm-global-mode 'maybe)
+
+	;; Highlight the embedded major mode with a dark green background
+	(set-face-attribute 'mmm-default-submode-face nil :background "#112f11")
+	)
