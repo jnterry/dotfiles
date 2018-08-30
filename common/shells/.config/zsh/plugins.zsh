dbb8127d35dc6737dcf6e1f04dd1efe33866f020
Jamie Terry <jnterry@ntlworld.com>
2018-08-30 13:34:19 +0100

    Add zsh autosuggestion and syntax highlighting plugins

---
+++ common/shells/.config/zsh/plugins.zsh
@@ -0,0 +1,8 @@
+echo "Sourcing plugins"
+
+pushd $(dirname "${0}") > /dev/null
+SCRIPTDIR=$(pwd -L)
+popd > /dev/null
+
+source ${SCRIPTDIR}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ${SCRIPTDIR}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
