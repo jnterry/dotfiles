577a582df3e087ff87a92f455a7767423175c9a0
Jamie Terry <jnterry@ntlworld.com>
2018-11-01 14:08:47 +0000

    Add terminal keymap

---
+++ common/emacs/.emacs.d/config/terminal-keymap.el
@@ -0,0 +1,16 @@
+(define-key input-decode-map "\e[1;2A" [S-up])
+(define-key input-decode-map "\e[1;2B" [S-down])
+(define-key input-decode-map "\e[1;2C" [S-right])
+(define-key input-decode-map "\e[1;2D" [S-left])
+
+(define-key input-decode-map "\e[1;5A" [C-up])
+(define-key input-decode-map "\e[1;5B" [C-down])
+(define-key input-decode-map "\e[1;5C" [C-right])
+(define-key input-decode-map "\e[1;5D" [C-left])
+
+(define-key input-decode-map "\e[15~" [f5])
+(define-key input-decode-map "\e[17~" [f5])
+(define-key input-decode-map "\e[19~" [f7])
+(define-key input-decode-map "\e[19~" [f8])
+(define-key input-decode-map "\e[20~" [f9])
+(define-key input-decode-map "\e[24~" [f12])
