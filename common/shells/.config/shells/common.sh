74712b1b817cd526f1ca7a124b5987651794134d
Jamie Terry <jnterry@ntlworld.com>
2018-12-28 00:35:55 +0000

    Fix loading of nvm

--- common/shells/.config/shells/common.sh
+++ common/shells/.config/shells/common.sh
@@ -13,8 +13,7 @@ source "$HOME/bin/k.sh"
 # added by travis gem
 [ -f /home/jamie/.travis/travis.sh ] && source /home/jamie/.travis/travis.sh
 
-
-if which nvm > /dev/null 2> /dev/null ; then
+if stat "$HOME/.nvm" > /dev/null 2> /dev/null ; then
 		export NVM_DIR="$HOME/.nvm"
 		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
