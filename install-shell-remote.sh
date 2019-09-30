6fdd57305f5eb61ee317fe3d104aee417e27e4eb
Jamie Terry <jnterry@ntlworld.com>
2019-09-30 13:02:59 +0100

    Add script to copy shell config to remote host

---
+++ install-shell-remote.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTDIR=$(dirname $SCRIPT)
+
+if [[ $# -ne 1 ]] ; then
+		echo "Usage: $0 [REMOTE_HOST]"
+		exit 1
+fi
+
+REMOTE=$1
+
+echo "Install shell config to $REMOTE"
+
+scp -pr $SCRIPTDIR/common/shells/bin $REMOTE:~/bin
+scp -pr $SCRIPTDIR/common/shells/.zshrc $REMOTE:~/.zshrc
+scp -pr $SCRIPTDIR/common/shells/.bashrc $REMOTE:~/.bashrc
+scp -pr $SCRIPTDIR/common/shells/.config $REMOTE:~/.config
