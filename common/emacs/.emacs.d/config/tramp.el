b3bef6d4ff00a428238f70216f3c3fb9fb5226d7
Jamie Terry <jt@terry.cloud>
2020-12-10 17:52:31 +0000

    [emacs] Fix bug where tramp doesn't work on machines which print last login time

--- common/emacs/.emacs.d/config/tramp.el
+++ common/emacs/.emacs.d/config/tramp.el
@@ -5,3 +5,12 @@
 ;; Use custom terminal type so we can detect it in zsh config and use simpler
 ;; prompt style compatible with tramp
 (setq tramp-terminal-type "tramp")
+
+;; Use custom prompt pattern
+;; The default matches the "last login XXX" line printed on many machines,
+;; which then causes tramp to hang
+;; See: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=147594
+(setq tramp-shell-promp-pattern "^[^$>%]*[$>%] *")
+
+;; Enable all debugging messages (will go in own buffer)
+;; (setq tramp-verbose 6)
