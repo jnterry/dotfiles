602bacf958576a3f9c976befba3f0067ebcde47e
Jamie Terry <dev@terry.cloud>
2024-04-14 19:09:17 +0100

    Fix install.sh on debian 12

--- setup/install.sh
+++ setup/install.sh
@@ -3,10 +3,16 @@
 
 set -e
 
+sudo apt update
+
+sudo apt install --yes fontconfig
+
 # Copy file structure
 cp -r ./files /
 /usr/bin/fc-cache -f -v # update font cache (fonts installed by above cp)
 mkdir -p /etc/network/interfaces.d
+
+sudo apt update
 
 # Add something like this to /etc/network/interfaces.d/${interface} replacing wlp9s0 with correct device
 #
@@ -22,7 +28,7 @@ sudo apt update
 sudo apt install --yes build-essential git gpg vim emacs zsh perl stow ca-certificates wget curl gnupg lsb-release software-properties-common apt-transport-https resolvconf htop bmon
 
 # networking
-sudo apt install --yes firmwire-realtek wpasupplicant ifupdown iproute2
+sudo apt install --yes firmware-realtek wpasupplicant ifupdown iproute2
 
 # audio
 sudo apt install --yes pulseaudio pavucontrol
@@ -31,7 +37,7 @@ sudo apt install --yes pulseaudio pavucontrol
 sudo apt install --yes xserver-xorg-video-amdgpu xserver-xorg-video-amdgpu mesa-utils libgl1-mesa-dri firmware-amd-graphics firmware-linux-nonfree
 
 # i3 build dependencies
-sudo apt install dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
+sudo apt install --yes dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
 
 # i3 / windows manager
 sudo apt install --yes i3status i3lock xorg xterm compton feh rofi scrot xclip terminator polybar
