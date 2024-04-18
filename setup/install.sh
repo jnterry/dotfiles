82e3242d8280c7c17ae302f30d3057b3295178ba
root <root@topaz.terry.cloud>
2024-04-18 17:55:16 +0100

    Further fixes to setup install script

--- setup/install.sh
+++ setup/install.sh
@@ -10,12 +10,17 @@ git submodule init
 git submodule update
 
 # dependencies for this script (and useful to keep on system anyway...)
-sudo apt install --yes fontconfig rsync curl
+sudo apt install --yes fontconfig rsync curl gnupg
 
 # Copy file structure
 sudo rsync -r ./files/* /
 /usr/bin/fc-cache -f -v # update font cache (fonts installed by above cp)
 mkdir -p /etc/network/interfaces.d
+sudo chmod +x /usr/bin/stowforce
+sudo chmod +x /usr/bin/i3screenshot
+sudo chmod +x /usr/bin/i3screenshot-full
+sudo chmod +X /usr/bin/i3emptyworkspace
+sudo chmod +x /usr/bin/git-change-origin
 
 # Import apt keys (new repos added with rsync above)
 sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com --recv-keys E88979FB9B30ACF2 # google chrome
@@ -36,6 +41,10 @@ sudo apt update
 # system tools
 sudo apt install --yes build-essential git gpg vim emacs zsh perl stow ca-certificates wget curl gnupg lsb-release software-properties-common apt-transport-https resolvconf htop bmon
 
+# Installing resolvconf above causes DNS resolution to fail, so restart networking to fix
+# and allow further package installations to work
+sudo systemctl restart networking
+
 # networking
 sudo apt install --yes firmware-realtek wpasupplicant ifupdown iproute2
 
@@ -44,11 +53,6 @@ sudo apt install --yes pulseaudio pavucontrol
 
 # gpu
 sudo apt install --yes xserver-xorg-video-amdgpu xserver-xorg-video-amdgpu mesa-utils libgl1-mesa-dri firmware-amd-graphics firmware-linux-nonfree
-
-# i3 build dependencies
-# sudo apt install --yes dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
-
-# i3 gaps now merged into apt pakcaged i3-wm, so we can just install it directly rather than building i3-gaps from source...
 
 # i3 / windows manager
 sudo apt install --yes i3-wm i3status i3lock xorg xterm compton feh rofi scrot xclip terminator polybar
